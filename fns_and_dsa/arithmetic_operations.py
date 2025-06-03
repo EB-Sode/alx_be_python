4c2eb9a7909c47039b03de3b07330616429b40ce
Blessed Sode <emi.square23@gmail.com>
2025-06-03 21:44:14 +0100

    update the operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,8 @@ def perform_operation(num1, num2, operation):
             case 'divide':
                 if num2 == 0: 
                     return "num2 cannot be zero for division."
+                elif num1 == 0:
+                    return num1 / num2
                 else:
                     return num1 / num2
             case _:
