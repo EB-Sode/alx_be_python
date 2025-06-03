88fb399f09c7c81f803536aec0afc04b89bb2b72
Blessed Sode <emi.square23@gmail.com>
2025-06-03 22:32:17 +0100

    Check current date and time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+
+def display_current_datetime(): 
+    # Get current date and time
+    current_date = datetime.now()
+    print("Today's date is:", current_date)
+
+def calculate_future_date():
+    # Add 7 days (1 week) to get a future date
+    current_date = datetime.now()
+    number_of_days = int(input("enter a number of days as an integer: "))
+    future_date = current_date + timedelta(days=number_of_days)
+
+    print(f"{number_of_days} days from now will be:", future_date.strftime("%Y-%m-%d"))
+
+
+display_current_datetime()
+calculate_future_date()
