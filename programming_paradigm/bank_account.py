a576704c38873195700357290055621ae2d27f9f
Blessed Sode <emi.square23@gmail.com>
2025-06-10 23:44:53 +0100

    updated bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount():
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+            return False
+        else:
+            self.account_balance - amount
+            return True
+            
+
+
+    def display_balance(self):
+        print({self.account_balance})
+
+
+
+
+
