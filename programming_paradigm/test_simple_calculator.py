2b6e2bfc4f12e092e479e76eaa642c920ba026c9
Blessed Sode <emi.square23@gmail.com>
2025-06-11 22:02:29 +0100

    adding Calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,21 +16,21 @@ class TestSimpleCalculator(unittest.TestCase):
         with self.assertRaises(NameError):
             self.calc.add(undefined_input, 3)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method"""
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(20, 30), -10)
         with self.assertRaises(TypeError):
             self.calc.subtract("string", 5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         '''Test multiplication method'''
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(7, 8), 56)
         with self.assertRaises(TypeError):
             self.calc.multiply("string", 5)
 
-    def test_divide(self):
+    def test_division(self):
         '''Test the division method'''
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertIsNone(self.calc.divide(10, 0))
