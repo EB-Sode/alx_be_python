3b8f9cc63d62dc89522334fa427d14ba36e7004f
Blessed Sode <emi.square23@gmail.com>
2025-06-11 05:47:20 +0100

    adding division with errors

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+#Calculator function
+
+def safe_divide(numerator, denominator):
+    '''Safe division while handling errors'''
+    try:
+        result = int(numerator) / int(denominator)
+        return f'The result of the division is {result}'
+    
+    #catch zero division
+    except ZeroDivisionError:
+        return 'Error: Cannot divide by zero.'
+    #catch invalid values
+    except ValueError:
+        return 'Error: Please enter numeric values only.'
